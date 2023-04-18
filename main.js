af8ca76a3bcb6420b78152f90959c8cfd9d3ac6e
Juan Vanegas <eatteer@gmail.com>
2023-04-17 20:14:47 -0500

    Some linting

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
+
 app.get("/", (_, res) => {
   res.send("root");
 });
+
 app.get("/go", (_, res) => {
   const redirectToUrl = "https://youtu.be/dQw4w9WgXcQ";
   res.redirect(redirectToUrl);
 });
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
