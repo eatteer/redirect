c2f04e92e1e9c24c04c3fc089b3ed9027a2f3900
Juan Vanegas <eatteer@gmail.com>
2023-04-17 20:11:35 -0500

    Implement redirect: go endpoint

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const app = express();
 app.get("/", (_, res) => {
   res.send("root");
 });
+app.get("/go", (_, res) => {
+  const redirectToUrl = "https://youtu.be/dQw4w9WgXcQ";
+  res.redirect(redirectToUrl);
+});
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
